7648c7e2c03e02846a26a0d5cbb7aaad663385b9
jasn4real <tobeobi@pursuit.org>
2022-10-16 15:57:58 -0400

    Add drinks and env for API key

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  console.log(process.env.REACT_APP_API_KEY);
+
+  const [drinks, setDrinks] = useState([]);
+
+  function getDrinkOnClick() {
+  
+  fetch("https://the-cocktail-db.p.rapidapi.com/popular.php", {
+    method: "GET",
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+		'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+    }
+  }).then(response => response.json()).then(data => {
+    console.log(data);
+    setDrinks(data.drinks);
+  });
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getDrinkOnClick}>Get Drinks</button>
+      {
+        drinks.map(d => {
+          return (
+            <div>
+              { d.strDrink }
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
